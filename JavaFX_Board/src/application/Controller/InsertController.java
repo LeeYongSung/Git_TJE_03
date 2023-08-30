34ccc2947dacc6681155a2f9d65d047b655b94f9
Kimjunhyuk0320 <kimminuk0320@gmail.com>
2023-08-30 18:37:58 +0900

    수정

--- JavaFX_Board/src/application/Controller/InsertController.java
+++ JavaFX_Board/src/application/Controller/InsertController.java
@@ -3,6 +3,8 @@ import java.io.IOException;
 
 import application.DAO.BoardDAO;
 import application.DTO.Board;
+import application.Service.BoardService;
+import application.Service.BoardServiceImpl;
 import application.Util.SceneUtil;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -22,7 +24,7 @@ public class InsertController {
     private TextArea textFieldContent;
      			
     BoardDAO boardDAO = new BoardDAO();	// 데이터베이스 연결 및 쿼리 실행 객체 생성
-
+    BoardService boardservice = new BoardServiceImpl();
     Board writeBoard;	// 새로운 게시판 객체 생성 (아직 초기화되지 않음)
     Stage stage;		// 스테이지 변수 선언 (아직 초기화되지 않음)
     Scene scene;		// 씬 변수 선언 (아직 초기화되지 않음)
@@ -38,7 +40,7 @@ public class InsertController {
         writeBoard.setTitle(textFieldTitles.getText());			// 제목 필드의 텍스트를 가져와 Board 객체에 설정   	
         writeBoard.setContent(textFieldContent.getText());		// 내용 필드의 텍스트를 가져와 Board 객체에 설정
         writeBoard.setWriter(textFieldWriter.getText());		// 작성자 필드의 텍스트를 가져와 Board 객체에 설정
-        boardDAO.insert(writeBoard);							// 생성한 게시글 정보가 담긴 Board 객체를 DB에 저장하는 DAO 메소드 호출
+        boardservice.insert(writeBoard);							// 생성한 게시글 정보가 담긴 Board 객체를 DB에 저장하는 DAO 메소드 호출
 
         /* SceneUtil을 사용하여 이벤트 발생 시 (글 작성 후 '작성' 버튼 클릭) 
         MAIN 화면으로 전환. UI.MAIN.getPath()는 MAIN 화면의 경로를 반환 */
